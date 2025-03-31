2646f3bf672c52e3e8b9eac01fa766d08065ee8c
Juan Pimentel <juandbpimentel@gmail.com>
2025-03-31 19:16:41 -0300

    fix: corrigindo teste

--- etapa_sistema_e_api/backend/api_test.py
+++ etapa_sistema_e_api/backend/api_test.py
@@ -2,5 +2,5 @@ import requests
 
 def test_status():
     response = requests.get("http://localhost:8080/")
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert response.json() == {"message": "Bem-vindo ao backend FastAPI com PostgreSQL!"}
