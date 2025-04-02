58825d6b9bd357a016b0986741151b41d65e5c9c
Juan Pimentel <juandbpimentel@gmail.com>
2025-04-02 16:33:47 -0300

    fix

--- etapa_sistema_e_api/backend/database.py
+++ etapa_sistema_e_api/backend/database.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from google.cloud.sql.connector import Connector
+from dotenv import load_dotenv
 
 import os
 
