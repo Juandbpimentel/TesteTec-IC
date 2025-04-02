34dbed7c08af1fe78b43ee474af1569f546d0e57
Juan Pimentel <juandbpimentel@gmail.com>
2025-04-02 16:43:43 -0300

    fix

--- etapa_sistema_e_api/backend/main.py
+++ etapa_sistema_e_api/backend/main.py
@@ -2,7 +2,6 @@ import os
 import platform
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.middleware.wsgi import WSGIMiddleware
 from dotenv import load_dotenv
 
 # Carrega variáveis de ambiente no modo de desenvolvimento
@@ -14,8 +13,6 @@ from logging_config import log_exceptions_middleware, setup_logging
 from database import engine, Base
 from routes import demonstracoes_routes, operadoras_routes
 
-# Configura o banco de dados
-Base.metadata.create_all(bind=engine)
 
 # Cria a aplicação FastAPI
 fastapi_app = FastAPI(title="Backend FastAPI com PostgreSQL")
@@ -27,7 +24,7 @@ setup_logging()
 fastapi_app.middleware("http")(log_exceptions_middleware)
 fastapi_app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "*").split(","),
+    allow_origins=os.getenv("CORS_ORIGIN_WHITELIST", "*").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -42,15 +39,10 @@ fastapi_app.include_router(demonstracoes_routes.router)
 def read_root():
     return {"message": "Bem-vindo ao backend FastAPI com PostgreSQL!"}
 
-# Adiciona suporte WSGI para Gunicorn
-application = WSGIMiddleware(fastapi_app)
+# Para compatibilidade com o App Engine Standard
+application = fastapi_app  # <--- Nome obrigatório
 
-# Executa o servidor com base no sistema operacional
+# Execução local
 if __name__ == "__main__":
-    if platform.system() == "Windows":
-        # Executa com Uvicorn no Windows
-        import uvicorn
-        uvicorn.run("main:fastapi_app", host="0.0.0.0", port=8080)
-    else:
-        # Executa com Gunicorn no Linux
-        os.system("gunicorn --bind :8080 --workers 1 --timeout 600 main:application")
+    import uvicorn
+    uvicorn.run("main:fastapi_app", host="0.0.0.0", port=8080)
